6b3353c38708abff85c62e84005090ca7463f969
Dane Miller <khadane.miller@gmail.com>
2021-09-03 00:13:18 -0400

    this for sure has to work

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,11 +2,4 @@
 
 cd /home/ec2-user/app
 
-pm2 describe appname > /dev/null
-RUNNING=$?
-
-if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start npm --name stockx -- run start
-else
-  pm2 stop stockx
-fi;
+pm2 stop --silent stockx
