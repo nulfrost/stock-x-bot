f8c09f29688b50bc44a3895d210764a9ea1798d8
Dane Miller <khadane.miller@gmail.com>
2021-09-02 23:56:13 -0400

    another attempt at fixing my stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,4 +2,11 @@
 
 cd /home/ec2-user/app
 
-pm2 stop stockx || true
+pm2 describe appname > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start npm --name stockx -- run start
+else
+  pm2 stop stockx
+fi;
