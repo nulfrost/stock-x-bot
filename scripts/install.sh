9179b6cdd0efec8f36081b6501b61a445fb1d968
Dane Miller <khadane.miller@gmail.com>
2021-09-04 21:29:43 -0400

    remove yarn install step from install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,5 @@
 set -e
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 yum install -y nodejs
-yum install -y yarn
 npm install -g pm2
