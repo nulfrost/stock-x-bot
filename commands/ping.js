9eb18e13bdd11d9fad5c0fc216a50ebe8e791611
Dane Miller <khadane.miller@gmail.com>
2021-09-22 18:30:20 -0400

    added a delete step to pm2 stop and start instructions

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription("Replies with Pong!"),
   async execute(interaction) {
     const sent = await interaction.reply({
-      content: "Pinging...",
+      content: "Pinging..",
       fetchReply: true,
     });
     interaction.editReply(
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,3 +3,4 @@
 cd /home/ec2-user/app
 
 pm2 stop --silent stockx
+pm2 delete --silent stockx
